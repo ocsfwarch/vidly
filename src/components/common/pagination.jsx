6456542533da891126d17789ac0bf396a817102e
ocsfwarch <tpartcal@gmail.com>
2023-11-10 09:32:30 -0800

    Adds Pagination component

---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import _ from 'lodash';
+
+const Pagination = (props) => {
+    const { itemsCount, pageSize, currentPage, onPageChange } = props;
+    const pagesCount = Math.ceil(itemsCount / pageSize);
+    if (pagesCount === 1) return null
+    const pages = _.range(1, pagesCount + 1)
+    return ( <nav aria-label="Page navigation example">
+    <ul className="pagination">
+        {pages.map((page) => <li key={page} className={ page === currentPage ? 'page-item active' : 'page-item'}><a onClick={() => onPageChange(page)} className="page-link" >{page}</a></li>)}         
+    </ul>
+  </nav>);
+}
+ 
+export default Pagination;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from './common/pagination'
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = (movie) => {
@@ -19,6 +22,10 @@ class Movies extends Component {
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
+
+  handlePageChange = (page) => {
+    this.setState({currentPage: page})
+  }
 
   render() {
     const { length: totalMovies } = this.state.movies;
@@ -65,6 +72,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination 
+          itemsCount={totalMovies} 
+          pageSize={this.state.pageSize} 
+          currentPage={this.state.currentPage}
+          onPageChange={this.handlePageChange} >
+        </Pagination>
       </React.Fragment>
     );
   }
